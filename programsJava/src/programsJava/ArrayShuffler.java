b1ee53e7489d41bfc2a84c1d934b0fadca635dae
vemulakalyani <vemulakalyani508@gmail.com>
2023-10-24 22:30:05 +0530

    initial commit

---
+++ programsJava/src/programsJava/ArrayShuffler.java
@@ -0,0 +1,21 @@
+package programsJava;
+import java.util.Arrays;
+import java.util.Random;
+public class ArrayShuffler {
+	public static void main(String[] args) {
+		 int[] arr = {8, 12, 7, 25, 54, 60, 77};
+		 shuffleArray(arr);
+		 System.out.println("Shuffled array: " + Arrays.toString(arr));
+		    }
+   private static void shuffleArray(int[] arr) {
+		     Random random = new Random();
+		       for (int i = arr.length - 1; i > 0; i--) {
+		          int j = random.nextInt(i + 1);
+		          int temp = arr[i];
+		           arr[i] = arr[j];
+		           arr[j] = temp;
+		 }
+	}
+
+}
+
---
+++ programsJava/src/programsJava/Pangram.java
@@ -0,0 +1,28 @@
+package programsJava;
+
+public class Pangram {
+	public static void main(String[] args) {		   
+	    String sentence = "Pack my box with five dozen liquor jugs";
+        boolean isPangram = checkIfPangram(sentence);
+	    System.out.println(isPangram);
+	    }
+	    public static boolean checkIfPangram(String s) {
+	        boolean[] alphabetPresent = new boolean[26];
+	        int count = 0;
+
+	        for (int i = 0; i < s.length(); i++) {
+	            char ch = s.charAt(i);
+	            if (Character.isLetter(ch)) {
+	                if (!alphabetPresent[Character.toLowerCase(ch) - 'a']) {
+	                    alphabetPresent[Character.toLowerCase(ch) - 'a'] = true;
+	                    count++;
+	                }
+            }
+        }
+        return count == 26;
+	    }
+}
+
+
+
+
